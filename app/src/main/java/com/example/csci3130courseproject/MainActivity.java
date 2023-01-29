ab5d651a09ba99b0a37519ad859efd153639730b
Alexander Elias <al366388@dal.ca>
2023-01-28 22:15:09 -0400

    Initial project file

---
+++ app/src/main/java/com/example/csci3130courseproject/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.csci3130courseproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    // Test comment
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
