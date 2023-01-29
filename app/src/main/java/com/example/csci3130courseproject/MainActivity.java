50a65c4219ac0e90439f1df646d2e0319e48f00e
Aryah <ar341476@dal.ca>
2023-01-29 18:50:30 -0400

    basic sign up prototype

--- app/src/main/java/com/example/csci3130courseproject/MainActivity.java
+++ app/src/main/java/com/example/csci3130courseproject/MainActivity.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
     // Test comment
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.sign_up);
+
+
     }
 }
