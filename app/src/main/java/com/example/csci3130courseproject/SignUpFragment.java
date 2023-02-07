0f11bac6b434202cc1931e0f89ade7617ad7cea4
Aryah <ar341476@dal.ca>
2023-02-07 15:26:40 -0400

    basic sign up without user input

--- app/src/main/java/com/example/csci3130courseproject/SignUpFragment.java
+++ app/src/main/java/com/example/csci3130courseproject/SignUpFragment.java
@@ -2,11 +2,23 @@ package com.example.csci3130courseproject;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +35,7 @@ public class SignUpFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    FragmentActivity activity;
 
     public SignUpFragment() {
         // Required empty public constructor
@@ -58,9 +71,31 @@ public class SignUpFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        activity = getActivity();
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_sign_up, container, false);
     }
 
-    public view
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+
+        mAuth.createUserWithEmailAndPassword("aryah2@email.com", "password").addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+//                    Log.i("signup", "");
+                    Toast.makeText(activity, "Signed Up", Toast.LENGTH_SHORT).show();
+                } else {
+//                    Log.i("signup", "");
+                    Toast.makeText(activity, "Sign Up failed", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+    }
 }
