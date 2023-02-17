c60bce21e1c077601c10237e27156ef8cfedbf95
Aryah <ar341476@dal.ca>
2023-02-17 13:45:49 -0400

    added test to see ifs earch page is visible

--- app/src/androidTest/java/com/example/csci3130courseproject/JobSearchTest.java
+++ app/src/androidTest/java/com/example/csci3130courseproject/JobSearchTest.java
@@ -5,6 +5,17 @@ import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import androidx.test.core.app.ActivityScenario;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,6 +28,12 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class JobSearchTest {
+
+    @Before
+    public void launchMainActivity() {
+        ActivityScenario.launch(MainActivity.class);
+    }
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
@@ -25,7 +42,7 @@ public class JobSearchTest {
     }
 
     @Test
-    public void searchPage_visible() {
-
+    public void checkIfSearchPageVisible() {
+        onView(withId(R.id.searchPage)).check(matches(isDisplayed()));
     }
 }
