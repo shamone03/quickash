0ed9822952a6328930e3248fefaa7300bbaf0f4f
Samuel Garcia <sam.garcia@dal.ca>
2023-02-17 13:08:27 -0400

    added a button to access Create listing (temporary add)

--- app/src/main/java/com/example/csci3130courseproject/DashboardFragment.java
+++ app/src/main/java/com/example/csci3130courseproject/DashboardFragment.java
@@ -1,12 +1,17 @@
 package com.example.csci3130courseproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -54,11 +59,21 @@ public class DashboardFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dashboard, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        Button gotoCreateJP = (Button) getView().findViewById(R.id.gotoCreateJP);
+        gotoCreateJP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_dashboardFragment_to_createListingFragment);
+            }
+        });
     }
 }
