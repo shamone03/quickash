4b2a8bb4c4a0a532376ac2e2917714a730f93d5d
Samuel Garcia <sam.garcia@dal.ca>
2023-03-05 16:18:00 -0400

    Refactored Applicant button to prefab_view_job_applicant_preview.xml

--- app/src/androidTest/java/com/example/csci3130courseproject/ViewJobAsEmployerTest.java
+++ app/src/androidTest/java/com/example/csci3130courseproject/ViewJobAsEmployerTest.java
@@ -60,7 +60,7 @@ public class ViewJobAsEmployerTest {
 
     @Test
     public void button_isVisible() {
-        onView(withId(R.id.ViewJobEmployerSaveButton)).check(matches(isDisplayed()));
+        onView(withId(R.id.ViewJobEmployerEditButton)).check(matches(isDisplayed()));
     }
 
 }
