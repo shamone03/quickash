7c449756c3e2d9da7d60f8806d0e513ab2d8dd03
Kayleen <yj923466@dal.ca>
2023-03-05 16:42:16 -0400

    initialized fields and getters for viewJobasEmployer

--- app/src/main/java/com/example/csci3130courseproject/UI/User/ViewJobEmployer.java
+++ app/src/main/java/com/example/csci3130courseproject/UI/User/ViewJobEmployer.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
@@ -12,8 +15,13 @@ import androidx.annotation.Nullable;
 import com.example.csci3130courseproject.R;
 
 public class ViewJobEmployer extends Fragment {
+
+    Button saveEdit, applicants;
+    EditText jobDescription;
+    TextView jobTitle, applicantName;
+
     // required empty constructor
-    public ViewJobEmployer() {  }
+    public ViewJobEmployer() { }
 
     @Nullable
     @Override
@@ -27,4 +35,19 @@ public class ViewJobEmployer extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
     }
+
+    // returns string value representing applicant's name on profile
+    public String getJobApplicantName() {
+        return applicantName.getText().toString();
+    }
+
+    // returns String value representing job title
+    public String getJobTitle() {
+        return jobTitle.getText().toString();
+    }
+
+    public String getJobDescription() {
+        return jobDescription.getText().toString();
+    }
+
 }
