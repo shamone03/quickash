afbd7006f6e4ed6315e0e9cad312544ea2444ce9
Aryah <ar341476@dal.ca>
2023-03-05 15:52:40 -0400

    espresso test will automatically navigate to viewjobemployer page

--- app/src/androidTest/java/com/example/csci3130courseproject/ViewJobAsEmployerTest.java
+++ app/src/androidTest/java/com/example/csci3130courseproject/ViewJobAsEmployerTest.java
@@ -1,22 +1,51 @@
 package com.example.csci3130courseproject;
 
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openContextualActionModeOverflowMenu;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.swipeRight;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertEquals;
 
 import android.content.Context;
 
+import androidx.fragment.app.testing.FragmentScenario;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.contrib.DrawerMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import com.example.csci3130courseproject.UI.HomePage.ListingSearchFragment;
+
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 
 @RunWith(AndroidJUnit4.class)
 public class ViewJobAsEmployerTest {
+    static NavController navController;
+    FragmentScenario fragmentScenario;
+
     @Before
-    public void launchMainActivity() {
+    public void launchMainActivity() throws InterruptedException {
         ActivityScenario.launch(MainActivity.class);
+        onView(withId(R.id.Sign_In_Email)).perform(typeText("test@email.com"));
+        onView(withId(R.id.Sign_In_Password)).perform(typeText("password"));
+        closeSoftKeyboard();
+        onView(withId(R.id.Sign_In_Request)).perform(click());
+        Thread.sleep(5000); // wait to sign in
+
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.viewJobEmployer)).perform(click());
     }
 
     @Test
@@ -25,7 +54,7 @@ public class ViewJobAsEmployerTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.csci3130courseproject", appContext.getPackageName());
     }
-
+    @Test
     public void checkIfPageVisible() {
 
     }
