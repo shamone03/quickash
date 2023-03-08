22b73d2c334971977c6c17f808bae52e86f137c6
Adam Melvin <gloom@ta2bd.wpa.dal.ca>
2023-03-08 15:53:15 -0400

    Rating Bar Fragment is all set up. Now need to connect it to the user object's Rating in firebase and integrate it into navigation once the Job finished fragments are built.

---
+++ app/src/main/java/com/example/csci3130courseproject/UserRatingFragment.java
@@ -0,0 +1,47 @@
+package com.example.csci3130courseproject;
+
+import android.graphics.PorterDuff;
+import android.graphics.drawable.LayerDrawable;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class UserRatingFragment extends Fragment {
+    private RatingBar ratingBar;
+    private final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+
+    public UserRatingFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ratingBar = (RatingBar)getView().findViewById(R.id.ratingBar);
+        LayerDrawable stars=(LayerDrawable)ratingBar.getProgressDrawable();
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user_rating, container, false);
+    }
+
+    public void rate(View v){
+        TextView message = (TextView)getView().findViewById(R.id.message);
+        message.setText("You Rated :" +String.valueOf(ratingBar.getRating()));
+    }
+}
