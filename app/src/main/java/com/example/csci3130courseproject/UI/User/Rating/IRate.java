e10bdf5339e181f7891733f67773193993016a35
Adam Melvin <gloom@Adams-MacBook-Air.local>
2023-03-18 15:06:25 -0300

    Added some tests for rating users before working on the backend rating functions to follow Test Driven Developement.

--- app/src/main/java/com/example/csci3130courseproject/UI/User/Rating/IRate.java
+++ app/src/main/java/com/example/csci3130courseproject/UI/User/Rating/IRate.java
@@ -1,11 +1,12 @@
 package com.example.csci3130courseproject.UI.User.Rating;
 
+import com.example.csci3130courseproject.UI.User.IUser;
 import com.example.csci3130courseproject.Utils.UserObject;
 
 public interface IRate {
 
-    public double getUserRating();
+    public double getUserRating(IUser user);
 
-    public void updateUserRating(UserObject user);
+    public double updateUserRating(IUser user, double rating);
 
 }
--- app/src/test/java/com/example/csci3130courseproject/UserRatingTest.java
+++ app/src/test/java/com/example/csci3130courseproject/UserRatingTest.java
@@ -1,11 +1,19 @@
 package com.example.csci3130courseproject;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.example.csci3130courseproject.UI.User.IUser;
+import com.example.csci3130courseproject.UI.User.Rating.IRate;
 import com.example.csci3130courseproject.Utils.UserObject;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.Silent.class)
@@ -13,9 +21,22 @@ public class UserRatingTest {
 
     public UserObject currentUser;
 
+    @Mock
+    static IRate rating;
+    static IUser user;
+
     @BeforeClass
     public void setup() {
+        MockitoAnnotations.openMocks(IRate.class);
+    }
 
+    @Test
+    public void testRateUser() throws Exception {
+        rating = Mockito.mock(IRate.class);
+        Mockito.when(rating.getUserRating(user)).thenReturn(5.0);
+        assertTrue(rating.getUserRating(user) == 5.0);
+        Mockito.when(rating.updateUserRating(user, 3)).thenReturn(4.0);
+        Mockito.verify(rating, Mockito.atLeastOnce()).getUserRating(user);
     }
 
 }
