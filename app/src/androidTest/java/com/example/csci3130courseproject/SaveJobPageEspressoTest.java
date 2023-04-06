ad90fc5a2aded8d77f00979f98b98baf052929f9
Aryah <ar341476@dal.ca>
2023-04-06 18:37:08 -0300

    fixed sign in test
    added function that executes a shell command to grant location permission so that we won't have to deal with the dialog after signing in

--- app/src/androidTest/java/com/example/csci3130courseproject/SaveJobPageEspressoTest.java
+++ app/src/androidTest/java/com/example/csci3130courseproject/SaveJobPageEspressoTest.java
@@ -4,8 +4,12 @@ import static androidx.test.espresso.Espresso.*;
 import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.assertion.ViewAssertions.*;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 public class SaveJobPageEspressoTest extends UserProfileEspressoTest{
 
--- app/src/androidTest/java/com/example/csci3130courseproject/SignInTest.java
+++ app/src/androidTest/java/com/example/csci3130courseproject/SignInTest.java
@@ -1,21 +1,30 @@
 package com.example.csci3130courseproject;
 
 import android.content.Context;
+import android.os.Build;
 
 //import androidx.fragment.app.testing.FragmentScenario;
+import androidx.annotation.NonNull;
 import androidx.test.core.app.ActivityScenario;
+
+import static androidx.test.InstrumentationRegistry.getTargetContext;
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.isDialog;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
+import androidx.test.internal.platform.content.PermissionGranter;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -29,9 +38,13 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class SignInTest {
-
-//    @Rule
-//    public ActivityScenarioRule<MainActivity> myRule = new ActivityScenarioRule<>(MainActivity.class);
+    @Before
+    public void grantPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // grant the location permissions so we won't have to deal with the dialog when signed in
+            getInstrumentation().getUiAutomation().executeShellCommand("pm grant " + getTargetContext().getPackageName() + " android.permission.ACCESS_FINE_LOCATION");
+        }
+    }
 
     @Before
     public void launchMainActivity() {
@@ -41,7 +54,7 @@ public class SignInTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        Context appContext = getInstrumentation().getTargetContext();
         assertEquals("com.example.csci3130courseproject", appContext.getPackageName());
     }
 
@@ -55,6 +68,7 @@ public class SignInTest {
     public void userSignedIn() throws InterruptedException {
         onView(withId(R.id.Sign_In_Email)).perform(typeText("test@email.com"));
         onView(withId(R.id.Sign_In_Password)).perform(typeText("password"));
+        closeSoftKeyboard();
         onView(withId(R.id.Sign_In_Request)).perform(click());
         Thread.sleep(5000); // temporary fix, should use idlingresource or smthn
         onView(withId(R.id.searchBar)).check(matches(isDisplayed()));
