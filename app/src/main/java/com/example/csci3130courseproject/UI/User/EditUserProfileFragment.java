dfd95ced1719c699f8ef6310f13cd2dea540b462
Adam Melvin <gloom@Adams-MacBook-Air.local>
2023-04-06 19:51:02 -0300

    Edit profile now working as intended. Can now edit credit card number and CVV number for paypal integration.

--- app/src/main/java/com/example/csci3130courseproject/UI/User/EditUserProfileFragment.java
+++ app/src/main/java/com/example/csci3130courseproject/UI/User/EditUserProfileFragment.java
@@ -21,6 +21,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class EditUserProfileFragment extends Fragment {
@@ -28,6 +29,8 @@ public class EditUserProfileFragment extends Fragment {
     private FirebaseAuth mAuth;
     UserObject profileUser;
     FirebaseUser currentUser = getUser();
+    DatabaseReference userIDRef = FirebaseDatabase.getInstance().getReference()
+        .child("users").child(currentUser.getUid());
     private EditText emailField, passwordField, nameField, phoneNumberField, dateOfBirthField,
             locationField, preferredJobsField, creditCardField, creditCardCVVField, countryField,
             provinceField, cityField, addressField;
@@ -100,10 +103,8 @@ public class EditUserProfileFragment extends Fragment {
                             creditCardField.getText().toString().trim(), creditCardCVVField.getText().toString().trim(),
                             countryField.getText().toString().trim(), provinceField.getText().toString().trim(),
                             cityField.getText().toString().trim(), addressField.getText().toString().trim());
-//        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-//                .setDisplayName(nameField.getText().toString().trim()
-//                .updateEmail(emailField.getText().toString().trim())
-//                ).build()
+
+                    userIDRef.setValue(profileUser);
                 }
             }
         });
@@ -172,8 +173,8 @@ public class EditUserProfileFragment extends Fragment {
 
 
         /*
-            New On click listener which calls getUser() and changeUserValues.
-            changeUserValues takes in the currentUser, and the nameField, and changes the user name.
+            New On click listener which calls changeUserValues.
+            changeUserValues takes in the currentUser and changes the user name.
             If changeUserValues returns true, then a Toast saying details changed successfully,
             Else error called, and a Toast for the same.
          */
@@ -182,15 +183,10 @@ public class EditUserProfileFragment extends Fragment {
             public void onClick(View view) {
 
                 boolean changeResult = changeUserValues(currentUser);
-                profileUser.updateUser(nameField.getText().toString(), phoneNumberField.getText().toString(),
-                        dateOfBirthField.getText().toString(), creditCardField.getText().toString(),
-                        creditCardCVVField.getText().toString(), countryField.getText().toString(),
-                        provinceField.getText().toString(), cityField.getText().toString(),
-                        addressField.getText().toString());
 
                 //Toast for the result.
                 if(changeResult){
-                    Toast.makeText(getActivity(),"Details Changes Successfully", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getActivity(),"Details Changed Successfully", Toast.LENGTH_SHORT).show();
                     editUserProfileToUserProfile(view);
                 }else{
                     Toast.makeText(getActivity(),"Error Occurred. Try again!", Toast.LENGTH_SHORT).show();
