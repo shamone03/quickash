27f655c896e681569ddde433516aa8f5e2d66362
Adam Melvin <gloom@ta20f.wpa.dal.ca>
2023-04-09 19:54:56 -0300

    Got paypal Api functioning

--- app/src/main/java/com/example/csci3130courseproject/MainActivity.java
+++ app/src/main/java/com/example/csci3130courseproject/MainActivity.java
@@ -66,4 +66,5 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
 }
---
+++ app/src/main/java/com/example/csci3130courseproject/Utils/Config.java
@@ -0,0 +1,8 @@
+package com.example.csci3130courseproject.Utils;
+
+public class Config {
+
+    //paypal client id
+    public static final String PAYPAL_CLIENT_ID = "AU9R_njJM40MRDc860_k_wte3x0Qlav8eEbQhizkFMjNYJcWnUfCs3edGMuANzE_9imOX8P4EZwsv9FN";
+}
+
