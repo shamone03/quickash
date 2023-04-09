bc30cca5269f9504e855ba118c1cacbcc46061f7
Samuel Garcia <sam.garcia@dal.ca>
2023-04-09 15:55:52 -0300

    Created custom exceptions for Espresso testing & developed the espresso test for validating if a job has been saved and is visible on profile.

--- app/src/androidTest/java/com/example/csci3130courseproject/SaveJobPageEspressoTest.java
+++ app/src/androidTest/java/com/example/csci3130courseproject/SaveJobPageEspressoTest.java
@@ -1,12 +1,12 @@
 package com.example.csci3130courseproject;
 
-import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasSibling;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -19,14 +19,19 @@ import static org.junit.Assert.assertTrue;
 import android.os.SystemClock;
 
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.NoMatchingViewException;
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.contrib.DrawerActions;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.example.csci3130courseproject.CustomExceptions.AlreadySavedException;
+import com.example.csci3130courseproject.CustomExceptions.UnsavedException;
+
+import junit.framework.AssertionFailedError;
+
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.internal.matchers.Any;
 
 @RunWith(AndroidJUnit4.class)
 public class SaveJobPageEspressoTest {
@@ -34,6 +39,7 @@ public class SaveJobPageEspressoTest {
     public void setup() throws InterruptedException {
         ActivityScenario.launch(MainActivity.class);
         SignInTest.grantPermission();
+        signIn("test@email.com","password");
     }
 
     private void signIn(String user, String password){
@@ -43,21 +49,37 @@ public class SaveJobPageEspressoTest {
         SystemClock.sleep(3000);
         onView(withId(R.id.searchBar)).check(matches(isDisplayed()));
     }
-    @Test
-    public void saveJob() {
-        signIn("test@email.com", "password");
-        onView(
-                allOf(
-                        withId(R.id.saveButton), hasSibling(withText("Title: Save me! Part 2"))
-                )
+    private void saveJob() throws AlreadySavedException {
+        ViewInteraction job = onView(allOf(withId(R.id.saveButton), hasSibling(withText("Title: Save me! Part 2"))));
+        try {
+            job.check(matches(withText("SAVE")));
+            job.check(matches(isClickable()));
+            job.perform(click());
+            job.check(matches(withText("UNSAVE")));
+        } catch (AssertionError e){
+            throw new AlreadySavedException();
+        }
+    }
 
-                ).perform(scrollTo(), click());
-        SystemClock.sleep(2000);
+    private void unsaveJob() throws UnsavedException {
+        ViewInteraction job = onView(allOf(withId(R.id.saveButton), hasSibling(withText("Title: Save me! Part 2"))));
+        try {
+            job.check(matches(withText("UNSAVE")));
+            job.check(matches(isClickable()));
+            job.perform(click());
+            job.check(matches(withText("SAVE")));
+        } catch (AssertionError e){
+            throw new UnsavedException();
+        }
     }
 
     @Test
-    public void saveJobIsVisibleInProfile(){
-        saveJob();
+    public void saveJobIsVisibleInProfile() {
+        try {
+            saveJob();
+        } catch (AlreadySavedException e){
+            // Ignore and continue.
+        }
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
         onView(withId(R.id.userProfileFragment)).perform(click());
         SystemClock.sleep(3000);
@@ -66,4 +88,10 @@ public class SaveJobPageEspressoTest {
         SystemClock.sleep(3000);
         onView(allOf(withText("Title: Save me! Part 2"))).perform(scrollTo()).check(matches(isDisplayed()));
     }
+
+    @Test
+    public void unsaveJobFromListingView(){
+
+    }
+
 }
